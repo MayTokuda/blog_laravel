740b1ccd9627a27cf4c5f3e8dd82f58139869c4c
Tsutomu001 <tsutomuadachi8@gmail.com>
2022-07-24 15:12:28 +0900

    DB定義(仮)---users/articles/tags(マイグレーション実行は未完了)

--- database/migrations/2022_07_23_071254_create_tags_table.php
+++ database/migrations/2022_07_23_071254_create_tags_table.php
@@ -14,7 +14,9 @@ class CreateTagsTable extends Migration
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id')->comment('タグの番号');
+            $table->unsignedBigInteger('article_id')->comment('記事の番号(外部キー)');
+            $table->text('name')->comment('タグの名前');
             $table->timestamps();
         });
     }
