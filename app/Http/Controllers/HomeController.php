d3bb1e87d581684069bd7c7557e222fc1bf75a9f
Tsutomu001 <tsutomuadachi8@gmail.com>
2022-07-27 14:13:25 +0900

    HomeControllerの処理の名前定義

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,13 +16,93 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
+
     /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
+    // ブログ一覧表示画面--->画面あり
     public function index()
     {
         return view('home');
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // ブログ新規入力フォーム--->画面あり
+    public function create()
+    {
+        //
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    // 追加処理(createの登録ボタン)--->画面なし
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // ブログ詳細表示--->画面あり
+    public function show($id)
+    {
+        //
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // 変更フォーム(既存の値が入っている状態)--->画面あり
+    public function edit($id)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // 変更処理(editの更新ボタン)--->画面なし
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    // 削除処理(showの削除ボタン)--->画面なし
+    public function destroy($id)
+    {
+        //
     }
 }
