5520a2fb14ab81bff19e4baf51f2410b21be48d0
DaichiKurata <dicyokocyoko@gmail.com>
2022-08-04 14:37:36 +0900

    editのviewの仕様変更

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,26 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<form action="{{ route('update') }}" method="post">
-    @csrf 
-    <dl class="form-list">
-        <dt>画像</dt>
-        <dd><input type="file" name="image" accept="image/png,image/jpeg"></dd>
-        <dt>タイトル</dt>
-        <dd><input type="text" name="title"></dd>
-        <dt>タグ</dt>
-        <dd><input type="text" name="tag"></dd>
-        <dt>本文</dt>
-        <dd><textarea name="body"></textarea></dd>
-    </dl>
-    <button type="submit">投稿する</button>
-    <a href="{{ route('dashbord') }}">キャンセル</a>
-</form>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('編集画面') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('編集') }}
+                    <main class="container">
+                    <form action="{{ route('update') }}" method="post">
+                        @csrf 
+                        <dl class="form-list">
+                            <dt>画像</dt>
+                            <dd><input type="file" name="image" value="{{$article->image}}" accept="image/png,image/jpeg"></dd>
+                            <dt>タイトル</dt>
+                            <dd><input type="text" name="title" value="{{$article->title}}"></dd>
+                            <dt>タグ</dt>
+                            <dd><input type="text" name="tag" value="{{$article->tag}}"></dd>
+                            <dt>本文</dt>
+                            <dd><textarea name="body" value="{{$article->body}}">{{$article->body}}</textarea></dd>
+                        </dl>
+                        <button type="submit">投稿する</button>
+                        <a href="{{ route('dashbord') }}">キャンセル</a>
+                    </form>
+                    </main>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
