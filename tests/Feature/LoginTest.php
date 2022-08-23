29610961762358ff455569200df17cfcab3609fd
Tsutomu001 <tsutomuadachi8@gmail.com>
2022-08-23 15:56:06 +0900

    ログインテスト---間違ったパスワードの場合など計3つ

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -43,4 +43,59 @@ class LoginTest extends TestCase
         // このユーザーがログイン認証されているか
         $this->assertAuthenticatedAs($this->user);
     }
+
+    public function test_間違ったパスワードの場合()
+    {
+        // ログイン画面に遷移する
+        $response = $this->get('/login');
+        // ログイン画面のステータステストを行う
+        $response->assertStatus(200);
+
+        // パスワードが正しく無い状態でログイン
+        $response = $this->post('/login', ['email' => $this->user->email, 'password' => 'Test123']);
+        // リダイレクトで戻ってくる。
+        $response->assertStatus(302);
+        // リダイレクトで戻ってきた時はログインページにいる事
+        $response->assertRedirect('/login');
+        // 失敗しているので認証されていない事
+        $this->assertGuest();
+    }
+
+    public function test_ログアウトが正しくできるか()
+    {
+        // ログイン状態の作成
+        // テスト側でユーザーの認証状態を指定
+        $response = $this->actingAs($this->user);
+        // ログアウトボタンが存在するところに戻る
+        $response = $this->get('/dashbord');
+        // ステータステストを行う
+        $response->assertStatus(200);
+
+        // ログアウト処理をする(postアクションでログアウトに処理を飛ばす)
+        $this->post('logout');
+
+        // ログアウト出来たら200番が帰ってきているか
+        $response->assertStatus(200);
+
+        // ログインページにいる事
+        $response = $this->get('/login');
+        // ログイン画面のステータステストを行う
+        $response->assertStatus(200);
+
+        // 認証されていないことを確認
+        $this->assertGuest();
+    }
+
+    public function test_ログインしていれば登録ページに遷移できない()
+    {
+        // ユーザーをログイン状態にします。
+        $response = $this->actingAs($this->user);
+
+        // 登録ページにアクセスする
+        $response = $this->get('/register');
+
+        // ログイン画面のステータステストを行う
+        // ログイン状態にあるので、跳ね返されます。ページは存在するけどアクセスする権限が無いので302をステータスで期待します
+        $response->assertStatus(302);
+    }
 }
