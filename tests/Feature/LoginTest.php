7991dec06221ade7c63776caabd118f580cdb900
Tsutomu001 <tsutomuadachi8@gmail.com>
2022-08-24 21:40:21 +0900

    ログインに関するtest完了

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -44,6 +44,7 @@ class LoginTest extends TestCase
         $this->assertAuthenticatedAs($this->user);
     }
 
+
     public function test_間違ったパスワードの場合()
     {
         // ログイン画面に遷移する
@@ -60,6 +61,7 @@ class LoginTest extends TestCase
         // 失敗しているので認証されていない事
         $this->assertGuest();
     }
+
 
     public function test_ログアウトが正しくできるか()
     {
@@ -86,6 +88,7 @@ class LoginTest extends TestCase
         $this->assertGuest();
     }
 
+
     public function test_ログインしていれば登録ページに遷移できない()
     {
         // ユーザーをログイン状態にします。
@@ -94,8 +97,86 @@ class LoginTest extends TestCase
         // 登録ページにアクセスする
         $response = $this->get('/register');
 
-        // ログイン画面のステータステストを行う
-        // ログイン状態にあるので、跳ね返されます。ページは存在するけどアクセスする権限が無いので302をステータスで期待します
+        // 登録画面のステータステストを行う
+        // 登録した状態にあるので、跳ね返されます。ページは存在するけどアクセスする権限が無いので302をステータスで期待します
         $response->assertStatus(302);
+    }
+
+
+    public function test_Emailが重複していれば登録できない()
+    {
+        // 登録ページにアクセスする
+        $response = $this->get('/register');
+
+        // 登録画面のステータステストを行う
+        $response->assertStatus(200);
+
+        // $passwordのハッシュ化を行う
+        $password = bcrypt('password');
+        $user_data = [
+            'name' => '太郎',
+            //  emailを既に登録しているユーザーのものと同じものにする。(重複テスト)
+            'email' => $this->user->email,
+            // メールアドレスが確認された日時を格納
+            'email_verified_at' => now(),
+            // パスワード
+            'password' => $password,
+            // 確認パスワードも必要
+            'password_confirmation' => $password,
+            // 'remember_token'=ログイン状態を保持させる---ランダムな文字列を生成する
+            'remember_token' => \Str::random(10),
+        ];
+        // ログイン後に飛ばすアクションを定義する->ログイン認証を行うところ
+        $first_path = route('login');
+
+        // 設定したパス情報と、登録情報を持ってpostアクションでコントローラー処理に飛ばす。
+        $response = $this->post($first_path, $user_data);
+
+        // 跳ね返されて、求めているエラーメッセージがsessionに含まれているか確認する(assertSeeは効かない可能性がある)
+        $response->assertSessionHasErrorsIn("もうすでに登録されています"); 
+
+        // ページは存在するけどアクセスする権限が無いので302をステータスで期待します
+        $response->assertStatus(302);
+
+        // リダイレクトで登録ページ遷移している事
+        $response->assertRedirect(route('register'));
+    }
+
+
+    public function test_Passwordが8文字以下であれば登録できない()
+    {
+        // 登録ページにアクセスする
+        $response = $this->get('/register');
+
+        // 登録画面のステータステストを行う
+        $response->assertStatus(200);
+
+        // パスワードを1文字にする
+        $password = 'T';
+        $user_data = [
+            'name' => '太郎',
+            'email' => 'test@test',
+            'email_verified_at' => now(),
+            // パスワードは1文字('T')
+            'password' => $password,
+            // 確認パスワードも必要
+            'password_confirmation' => $password,
+            'remember_token' => \Str::random(10),
+        ];
+        // ログイン後に飛ばすアクションを定義する->ログイン認証を行うところ
+        $first_path = route('login');
+
+        // 設定したパス情報と、登録情報を持ってpostアクションでコントローラー処理に飛ばす。
+        $response = $this->post($first_path, $user_data);
+
+        // リダイレクトで跳ね返って来た時に求めているエラーメッセージがsessionに含まれているか確認します
+        $response->assertSessionHasErrorsIn("パスワードは8文字以上で入力してください");
+
+        // ページは存在するけどアクセスする権限が無いので302をステータスで期待します
+        $response->assertStatus(302);
+
+        // リダイレクトでページ遷移している事
+        $response->assertRedirect(route('register'));
+
     }
 }
