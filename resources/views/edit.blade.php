1c398f160007a0b86af7db80484d2c1f6b3dbd86
DaichiKurata <dicyokocyoko@gmail.com>
2022-09-08 15:59:08 +0900

    画像編集ボタンを元のレイアウトに戻しました

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -26,13 +26,11 @@
                                 @endif
                             </div>
 
-
-                            {{-- <dd><input type="file" name="image" value="{{$article->image}}" accept="image/png,image/jpeg"></dd> --}}
-
                             <img class="article-img show-img" src="{{ \Storage::url($article->image) }}" alt="" width="100%">
-                            <input type="file" name="image" id="file_photo" style="display:none;" value="{{$article->image}}" accept="image/png,image/jpeg">
+                            <!-- <input type="file" name="image" id="file_photo" style="display:none;" value="{{$article->image}}" accept="image/png,image/jpeg">
                             
-                            <dd><label class="img-label" for="file_photo">画像を選択</label></dd>
+                            <dd><label class="img-label" for="file_photo">画像を選択</label></dd> -->
+                            <dd><input type="file" name="image" value="{{$article->image}}" accept="image/png,image/jpeg"></dd>
 
                             <dt>タイトル</dt><div class="color-red">
                                 {{-- 本文のエラーメッセージ --}}
